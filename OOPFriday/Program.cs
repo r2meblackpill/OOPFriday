3426c377bdc12515b1129c60b4babdfa047657f6
Opilane TTHK <opilane@tthk.ee>
2020-11-27 09:13:37 +0200

    first

---
+++ OOPFriday/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OOPFriday
+{
+    class Cat
+    {
+        string name;
+        string color;
+        double hunger;
+
+        public Cat (string _name, string _color)
+        {
+            name = _name;
+            color = _color;
+            hunger = 1;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Color
+        {
+            get { return color; }
+        }
+        public double Hunger
+        {
+            get { return hunger; }
+        }
+        public void Meow()
+        {
+            Console.WriteLine("meow");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("The cat is sleeping.");
+            hunger -= 0.1;
+            Console.WriteLine("DEBUG: Hunger: " + hunger);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Miisu", "Black and white");
+
+            Console.WriteLine(newCat.Hunger);
+            while (newCat.Hunger !> 0)
+            {
+                newCat.Sleep();
+            }
+        }
+
+    }
+}
---
+++ Pet/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Pet
+{
+    class Pet
+    {
+        string name;
+        int age;
+        double weight;
+        int lifespan;
+        
+        public Pet(string _name, double _weight)
+        {
+            name = _name;
+            weight = _weight;
+        }
+        public int Lifespan
+        {
+            get { return lifespan; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+        }
+        public double Age
+        {
+            get { return age; }
+        }
+        public void Eat()
+        {
+            Console.WriteLine("DEBUG: Eat routine");
+            Console.WriteLine("DEBUG: Weight before: " + weight);
+            weight += 0.2;
+            Console.WriteLine("DEBUG: Weight after: " +weight);
+        }
+        public void Exercise()
+        {
+            Console.WriteLine("DEBUG: Exercise routine");
+            Console.WriteLine("DEBUG: Weight before: " + weight);
+            weight -= 0.1;
+            Console.WriteLine("DEBUG: Weight after: " + weight);
+        }
+        public void InfoDisplay()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Weight: " + weight);
+            Console.WriteLine("Lifespan: " + lifespan);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet newHamster = new Pet("Luud", 0.01);
+
+            for (int age = 0; age < 10; age++)
+            {
+                if (newHamster.Weight > 0.7)
+                {
+                    newHamster.Exercise();
+                }
+               else if (newHamster.Weight < 0.7)
+                {
+                    newHamster.Eat();
+                }
+                Console.WriteLine("Iteration: " + age);
+            }
+        }
+    }
+}
